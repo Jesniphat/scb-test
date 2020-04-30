c762643addbaee8c4ab840cbf20cc06dd8190aad
Jesniphat <jesniphat@hotmail.com>
2020-04-30 15:00:37 +0700

    update to 8.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'email', loadChildren: './modules/email/email.module#EmailModule'},
-  {path: 'login', loadChildren: './login/login.module#LoginModule'},
+  {path: 'email', loadChildren: () => import('./modules/email/email.module').then(m => m.EmailModule)},
+  {path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
   {path: '', redirectTo: 'email', pathMatch: 'full'}
 ];
 
